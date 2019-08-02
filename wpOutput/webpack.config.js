6d5a6ed535c26f4e43640973e1a40ebf7de600fd
feigebaobei <18515195415@163.com>
2019-08-02 20:27:59 +0800

    use manifest

--- wpOutput/webpack.config.js
+++ wpOutput/webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
   plugins: [
-    new CleanWebpackPlugin,
+    new ManifestPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
