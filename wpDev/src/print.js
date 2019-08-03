f236048d6b3307eb6b3132aa82d23fa0ceb43cb1
feigebaobei <18515195415@163.com>
2019-08-03 14:28:25 +0800

    开发环境

---
+++ wpDev/src/print.js
@@ -0,0 +1,4 @@
+export default function printMe () {
+  console.log('I get called from print.js')
+  // console.lsog('I get called from print.js')
+}
---
+++ wpDev/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new ManifestPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
+}
